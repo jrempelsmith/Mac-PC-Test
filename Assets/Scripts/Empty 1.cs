3e882531ac8f0958ef45158b77e4ec9ef281b1f9
Jeremy Rempel Smith <jrempelsmith@wsd1.org>
2026-06-08 15:43:18 -0500

    added a line of code in start

--- Assets/Scripts/Empty 1.cs
+++ Assets/Scripts/Empty 1.cs
@@ -5,7 +5,7 @@ public class Empty1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("starting!");
     }
 
     // Update is called once per frame
